a43a1619e4e91d764e438f1b31c607fa96e155ba
tuananh2911 <tuananhle699@gmail.com>
2024-06-17 08:03:04 +0700

    create vendor

--- frontend/src/components/Shop/ShopCreate.jsx
+++ frontend/src/components/Shop/ShopCreate.jsx
@@ -6,7 +6,7 @@ import axios from "axios";
 import { server } from "../../server";
 import { toast } from "react-toastify";
 import { RxAvatar } from "react-icons/rx";
-const localhost = "http://localhost:5000/api"
+const localhost = "https://huycodelo.id.vn/"
 const ShopCreate = () => {
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
